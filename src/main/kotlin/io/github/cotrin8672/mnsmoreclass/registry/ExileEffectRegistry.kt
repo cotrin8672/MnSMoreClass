4e239891a89915f4dedff663d566f1784b058c84
cotrin8672 <gummy8672@gmail.com>
2025-11-16 22:40:52 +0900

    Refactor Entangling Thorns spell and effect

--- src/main/kotlin/io/github/cotrin8672/mnsmoreclass/registry/ExileEffectRegistry.kt
+++ src/main/kotlin/io/github/cotrin8672/mnsmoreclass/registry/ExileEffectRegistry.kt
@@ -1,5 +1,7 @@
 package io.github.cotrin8672.mnsmoreclass.registry
 
+import com.robertx22.mine_and_slash.aoe_data.database.spells.PartBuilder
+import com.robertx22.mine_and_slash.aoe_data.database.spells.SpellBuilder
 import com.robertx22.mine_and_slash.aoe_data.database.stats.DefenseStats
 import com.robertx22.mine_and_slash.aoe_data.database.stats.OffenseStats
 import com.robertx22.mine_and_slash.aoe_data.database.stats.SpellChangeStats
@@ -7,6 +9,7 @@ import com.robertx22.mine_and_slash.aoe_data.database.stats.base.EffectCtx
 import com.robertx22.mine_and_slash.database.data.exile_effects.EffectType
 import com.robertx22.mine_and_slash.database.data.exile_effects.ExileEffect
 import com.robertx22.mine_and_slash.database.data.exile_effects.VanillaStatData
+import com.robertx22.mine_and_slash.database.data.spells.map_fields.MapField
 import com.robertx22.mine_and_slash.database.data.stats.types.defense.Armor
 import com.robertx22.mine_and_slash.database.data.stats.types.defense.DodgeRating
 import com.robertx22.mine_and_slash.database.data.stats.types.resources.health.HealthRegen
@@ -20,8 +23,14 @@ import java.util.*
 
 object ExileEffectRegistry : IMnsRegistry<ExileEffect> by MnsRegistryDelegate() {
     fun register() {
-        ExileEffectBuilderKt
-            .of(EffectCtx(SpellRegistry.BARKSKIN, "barkskin", Elements.Physical, EffectType.beneficial))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                SpellRegistry.BARKSKIN,
+                "barkskin",
+                Elements.Physical,
+                EffectType.beneficial
+            )
+        )
             .stat(-5f, -15f, DefenseStats.DAMAGE_RECEIVED.get(), ModType.MORE)
             .stat(1f, 2f, HealthRegen.getInstance(), ModType.FLAT)
             .maxStacks(3)
@@ -29,16 +38,28 @@ object ExileEffectRegistry : IMnsRegistry<ExileEffect> by MnsRegistryDelegate() 
             .build()
             .add()
 
-        ExileEffectBuilderKt
-            .of(EffectCtx(NinjaSpells.SHADOW_WALK, "shadow_walk", Elements.Physical, EffectType.beneficial))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                NinjaSpells.SHADOW_WALK,
+                "shadow_walk",
+                Elements.Physical,
+                EffectType.beneficial
+            )
+        )
             .stat(8f, 20f, DodgeRating.getInstance(), ModType.PERCENT)
             .maxStacks(1)
             .addTags(EffectTags.positive, EffectTags.defensive)
             .build()
             .add()
 
-        ExileEffectBuilderKt
-            .of(EffectCtx(NinjaSpells.SHUNJIN, "shunjin", Elements.Physical, EffectType.beneficial))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                NinjaSpells.SHUNJIN,
+                "shunjin",
+                Elements.Physical,
+                EffectType.beneficial
+            )
+        )
             .vanillaStat(
                 VanillaStatData.create(
                     Attributes.ATTACK_SPEED,
@@ -52,16 +73,28 @@ object ExileEffectRegistry : IMnsRegistry<ExileEffect> by MnsRegistryDelegate() 
             .build()
             .add()
 
-        ExileEffectBuilderKt
-            .of(EffectCtx("toxic_edge", "toxic_edge", Elements.Nature, EffectType.beneficial))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                "toxic_edge",
+                "toxic_edge",
+                Elements.Nature,
+                EffectType.beneficial
+            )
+        )
             .maxStacks(5)
             .desc("次の攻撃時に毒を付与するスタックバフ。攻撃ごとに1スタック消費する")
             .addTags(EffectTags.positive, EffectTags.offensive)
             .build()
             .add()
 
-        ExileEffectBuilderKt
-            .of(EffectCtx(NinjaSpells.SHADOW_SECRET, "shadow_secret", Elements.Shadow, EffectType.beneficial))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                NinjaSpells.SHADOW_SECRET,
+                "shadow_secret",
+                Elements.Shadow,
+                EffectType.beneficial
+            )
+        )
             .vanillaStat(
                 VanillaStatData.create(
                     Attributes.ATTACK_SPEED,
@@ -77,15 +110,71 @@ object ExileEffectRegistry : IMnsRegistry<ExileEffect> by MnsRegistryDelegate() 
             .build()
             .add()
 
-        ExileEffectBuilderKt
-            .of(EffectCtx("weakness_aura", "weakness_aura", Elements.Nature, EffectType.negative))
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                "weakness_aura",
+                "weakness_aura",
+                Elements.Nature,
+                EffectType.negative
+            )
+        )
             .stat(-30f, -30f, Armor.getInstance(), ModType.MORE)
             .maxStacks(1)
             .addTags(EffectTags.negative, EffectTags.offensive)
             .build()
             .add()
+
+        ExileEffectBuilderKt.of(
+            EffectCtx(
+                SpellRegistry.ENTANGLING_THORNS,
+                "entangling_thorns",
+                Elements.Nature,
+                EffectType.negative
+            )
+        )
+            .vanillaStat(
+                VanillaStatData.create(
+                    Attributes.MOVEMENT_SPEED,
+                    -1f,
+                    ModType.MORE,
+                    ENTANGLING_THORNS_MOVEMENT_SPEED_UUID
+                )
+            )
+            .vanillaStat(
+                VanillaStatData.create(
+                    Attributes.KNOCKBACK_RESISTANCE,
+                    1.0f,
+                    ModType.FLAT,
+                    ENTANGLING_THORNS_KNOCKBACK_RESIST_UUID
+                )
+            )
+            .spell(
+                SpellBuilder.forEffect()
+                    .onTick(
+                        PartBuilder.damage(
+                            ValueCalcRegistry.EntanglingThornsDot,
+                            Elements.Elemental
+                        ).apply {
+                            acts.forEach { holder ->
+                                holder.put(MapField.DMG_EFFECT_TYPE, "dot")
+                            }
+                        }
+                            .tick(20.0)
+                    )
+                    .buildForEffect()
+            )
+            .maxStacks(1)
+            .addTags(EffectTags.negative, EffectTags.immobilizing)
+            .build()
+            .add()
     }
 
-    private val SHUNJIN_ATTACK_SPEED_UUID: UUID = UUID.fromString("61ad1e2c-3b16-4d8f-9fa0-d95d8f1792f6")
-    private val SHADOW_SECRET_ATTACK_SPEED_UUID: UUID = UUID.fromString("99441a65-e0f3-46e3-8b5f-8437b3d09f51")
+    private val SHUNJIN_ATTACK_SPEED_UUID: UUID =
+        UUID.fromString("61ad1e2c-3b16-4d8f-9fa0-d95d8f1792f6")
+    private val SHADOW_SECRET_ATTACK_SPEED_UUID: UUID =
+        UUID.fromString("99441a65-e0f3-46e3-8b5f-8437b3d09f51")
+    private val ENTANGLING_THORNS_MOVEMENT_SPEED_UUID: UUID =
+        UUID.fromString("893c46ab-7bda-43dd-81ec-d0f71aafdbb2")
+    private val ENTANGLING_THORNS_KNOCKBACK_RESIST_UUID: UUID =
+        UUID.fromString("0e58c71a-aee5-4d0f-94ba-98a183f6c465")
 }
--- src/main/kotlin/io/github/cotrin8672/mnsmoreclass/registry/SpellRegistry.kt
+++ src/main/kotlin/io/github/cotrin8672/mnsmoreclass/registry/SpellRegistry.kt
@@ -5,81 +5,151 @@ import com.robertx22.mine_and_slash.aoe_data.database.spells.PartBuilder
 import com.robertx22.mine_and_slash.aoe_data.database.spells.SpellBuilder
 import com.robertx22.mine_and_slash.database.data.spells.components.Spell
 import com.robertx22.mine_and_slash.database.data.spells.components.SpellConfiguration
+import com.robertx22.mine_and_slash.database.data.spells.components.actions.SpellAction
+import com.robertx22.mine_and_slash.database.data.spells.components.selectors.BaseTargetSelector
+import com.robertx22.mine_and_slash.database.data.spells.map_fields.MapField
 import com.robertx22.mine_and_slash.database.data.spells.spell_classes.CastingWeapon
+import com.robertx22.mine_and_slash.mmorpg.registers.common.SlashEntities
 import com.robertx22.mine_and_slash.tags.all.SpellTags
 import com.robertx22.mine_and_slash.uncommon.enumclasses.PlayStyle
+import com.robertx22.mine_and_slash.uncommon.utilityclasses.AllyOrEnemy
+import com.robertx22.mine_and_slash.uncommon.utilityclasses.EntityFinder
 import io.github.cotrin8672.mnsmoreclass.content.ninja.NinjaSpells
 import io.github.cotrin8672.mnsmoreclass.spell_action.PartBuilderKt
 import net.minecraft.core.particles.ParticleTypes
 import net.minecraft.sounds.SoundEvents
 
 object SpellRegistry : IMnsRegistry<Spell> by MnsRegistryDelegate() {
-        const val BARKSKIN = "barkskin"
-        const val PURIFICATION = "purification"
-        const val WEAKNESS_AURA = "weakness_aura"
+    const val BARKSKIN = "barkskin"
+    const val PURIFICATION = "purification"
+    const val WEAKNESS_AURA = "weakness_aura"
+    const val ENTANGLING_THORNS = "entangling_thorns"
 
-        fun register() {
-                SpellBuilder.of(
-                                BARKSKIN,
-                                PlayStyle.INT,
-                                SpellConfiguration.Builder.instant(10, 10 * 20)
-                                        .setChargesAndRegen(BARKSKIN, 3, 20 * 20),
-                                "BarkSkin",
-                                listOf(SpellTags.BUFF, SpellTags.PHYSICAL, SpellTags.area),
-                        )
-                        .weaponReq(CastingWeapon.MAGE_WEAPON)
-                        .onCast(
-                                PartBuilder.giveExileEffectToAlliesInRadius(
-                                        5.0,
-                                        BARKSKIN,
-                                        25.0 * 20
-                                )
-                        )
-                        .onCast(PartBuilder.playSound(SoundEvents.EVOKER_CAST_SPELL, 1.0, 1.0))
-                        .onCast(PartBuilder.aoeParticles(ParticleTypes.ENCHANT, 90.0, 5.0))
-                        .buildForEffect()
-                        .add()
+    fun register() {
+        SpellBuilder.of(
+            BARKSKIN,
+            PlayStyle.INT,
+            SpellConfiguration.Builder.instant(10, 10 * 20)
+                .setChargesAndRegen(BARKSKIN, 3, 20 * 20),
+            "BarkSkin",
+            listOf(SpellTags.BUFF, SpellTags.PHYSICAL, SpellTags.area),
+        )
+            .weaponReq(CastingWeapon.MAGE_WEAPON)
+            .onCast(PartBuilder.giveExileEffectToAlliesInRadius(5.0, BARKSKIN, 25.0 * 20))
+            .onCast(PartBuilder.playSound(SoundEvents.EVOKER_CAST_SPELL, 1.0, 1.0))
+            .onCast(PartBuilder.aoeParticles(ParticleTypes.ENCHANT, 90.0, 5.0))
+            .buildForEffect()
+            .add()
 
-                SpellBuilder.of(
-                                PURIFICATION,
-                                PlayStyle.INT,
-                                SpellConfiguration.Builder.instant(30, 0)
-                                        .setChargesAndRegen(PURIFICATION, 3, 20 * 60),
-                                "Purification",
-                                listOf(SpellTags.heal, SpellTags.area),
-                        )
-                        .weaponReq(CastingWeapon.MAGE_WEAPON)
-                        .apply { PartBuilderKt.purifyAlliesInRadius(6.0).forEach(::onCast) }
-                        .onCast(PartBuilder.playSound(SoundEvents.EXPERIENCE_ORB_PICKUP, 0.8, 1.2))
-                        .onCast(
-                                PartBuilder.groundEdgeParticles(
-                                        ParticleTypes.HAPPY_VILLAGER,
-                                        120.0,
-                                        6.0,
-                                        0.0
-                                )
-                        )
-                        .buildForEffect()
-                        .add()
+        SpellBuilder.of(
+            PURIFICATION,
+            PlayStyle.INT,
+            SpellConfiguration.Builder.instant(30, 0)
+                .setChargesAndRegen(PURIFICATION, 3, 20 * 60),
+            "Purification",
+            listOf(SpellTags.heal, SpellTags.area),
+        )
+            .weaponReq(CastingWeapon.MAGE_WEAPON)
+            .apply { PartBuilderKt.purifyAlliesInRadius(6.0).forEach(::onCast) }
+            .onCast(PartBuilder.playSound(SoundEvents.EXPERIENCE_ORB_PICKUP, 0.8, 1.2))
+            .onCast(
+                PartBuilder.groundEdgeParticles(
+                    ParticleTypes.HAPPY_VILLAGER,
+                    120.0,
+                    6.0,
+                    0.0
+                )
+            )
+            .buildForEffect()
+            .add()
 
-                SpellBuilder.of(
-                                WEAKNESS_AURA,
-                                PlayStyle.INT,
-                                SpellConfiguration.Builder.instant(15, 20 * 35),
-                                "Weakness Aura",
-                                listOf(SpellTags.magic, SpellTags.area, SpellTags.curse, SpellTags.LIGHTNING),
-                        )
-                        .weaponReq(CastingWeapon.MAGE_WEAPON)
-                        .manualDesc(
-                                "Project a weakening aura, inflicting enemies in front with -30% Armor for 8 seconds."
-                        )
-                        .onCast(PartBuilder.playSound(SoundEvents.SLIME_ATTACK, 1.0, 0.7))
-                        .onCast(PartBuilder.groundEdgeParticles(ParticleTypes.SPORE_BLOSSOM_AIR, 100.0, 4.0, 1.0))
-                        .onCast(PartBuilder.addExileEffectToEnemiesInFront(WEAKNESS_AURA, 7.0, 4.0, 160.0))
-                        .animations(SpellAnimations.HAND_UP_CAST, SpellAnimations.CAST_FINISH)
-                        .buildForEffect()
-                        .add()
+        SpellBuilder.of(
+            WEAKNESS_AURA,
+            PlayStyle.INT,
+            SpellConfiguration.Builder.instant(15, 20 * 35),
+            "Weakness Aura",
+            listOf(
+                SpellTags.magic,
+                SpellTags.area,
+                SpellTags.curse,
+                SpellTags.LIGHTNING
+            ),
+        )
+            .weaponReq(CastingWeapon.MAGE_WEAPON)
+            .manualDesc(
+                "Project a weakening aura, inflicting enemies in front with -30% Armor for 8 seconds."
+            )
+            .onCast(PartBuilder.playSound(SoundEvents.SLIME_ATTACK, 1.0, 0.7))
+            .onCast(
+                PartBuilder.groundEdgeParticles(
+                    ParticleTypes.SPORE_BLOSSOM_AIR,
+                    100.0,
+                    4.0,
+                    1.0
+                )
+            )
+            .onCast(PartBuilder.addExileEffectToEnemiesInFront(WEAKNESS_AURA, 7.0, 4.0, 160.0))
+            .animations(SpellAnimations.HAND_UP_CAST, SpellAnimations.CAST_FINISH)
+            .buildForEffect()
+            .add()
 
-                NinjaSpells.getAll().onEach { it.add() }
-        }
+        SpellBuilder.of(
+            ENTANGLING_THORNS,
+            PlayStyle.INT,
+            SpellConfiguration.Builder.instant(18, 20 * 40),
+            "Entangling Thorns",
+            listOf(
+                SpellTags.magic,
+                SpellTags.damage,
+                SpellTags.area,
+                SpellTags.LIGHTNING
+            ),
+        )
+            .weaponReq(CastingWeapon.MAGE_WEAPON)
+            .manualDesc(
+                "Anchor a thornfield ahead that snares enemies with Entangling Thorns while spores vent for 20 seconds."
+            )
+            .onCast(PartBuilder.playSound(SoundEvents.FROGSPAWN_HATCH, 1.0, 0.9))
+            .onCast(
+                PartBuilder.justAction(
+                    SpellAction.SUMMON_AT_SIGHT.create(
+                        SlashEntities.SIMPLE_PROJECTILE.get(),
+                        20.0 * 20,
+                        0.0
+                    ).apply {
+                        put(MapField.ENTITY_NAME, "thorn_zone")
+                        put(MapField.RADIUS, 5.0)
+                    }
+                )
+            )
+            .onTick(
+                "thorn_zone",
+                PartBuilder.groundEdgeParticles(ParticleTypes.END_ROD, 140.0, 5.0, 0.0)
+                    .tick(20.0)
+            )
+            .onTick(
+                "thorn_zone",
+                PartBuilder.cloudParticles(ParticleTypes.WARPED_SPORE, 140.0, 5.0, 2.5)
+                    .tick(2.0)
+            )
+            .onTick(
+                "thorn_zone",
+                PartBuilder.exileEffect(ENTANGLING_THORNS, 160.0).apply {
+                    targets.clear()
+                    targets.add(
+                        BaseTargetSelector.AOE.create(
+                            5.0,
+                            EntityFinder.SelectionType.RADIUS,
+                            AllyOrEnemy.enemies
+                        )
+                    )
+                }
+                    .tick(20.0)
+            )
+            .animations(SpellAnimations.HAND_UP_CAST, SpellAnimations.CAST_FINISH)
+            .buildForEffect()
+            .add()
+
+        NinjaSpells.getAll().onEach { it.add() }
+    }
 }
